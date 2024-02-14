76e3b878eb233daa4a37ca59c7ff0d2b67baa743
pablotroncoso <pablotroncoso@gmail.com>
2024-02-14 16:22:07 -0300

    Subiendo archivos index.html y script.js

---
+++ assets/js/script.js
@@ -0,0 +1,64 @@
+//EDAD
+const edad;
+edad = prompt("Bienvenido, cuál es tu edad: ")
+if(edad >= 18) {
+alert("Muy bien eres mayor de edad para poder jugar al Cachipún");
+}
+else {
+alert("Aún no cumples con la edad requerida, intenta en unos años más.");
+}
+
+
+
+// Usuario Ingresa Piedra, Pepel o Tijera
+decide = prompt("Hola! Escribe el número que quieres jugar. 1 para Piedra, 2 para Papel o 3 para Tijera: ");
+
+const unopiedra;
+const dospapel;
+const trestijera;
+
+
+if(decide === comp) {
+  alert("Empate!");
+}
+else {
+    alert("Empate!");
+
+}
+else {
+    alert("Empate!");
+
+}
+else {
+    alert("Empate!");
+
+}
+else {
+    alert("Empate!");
+
+}
+else {
+    alert("Empate!");
+
+}
+else {
+    alert("Empate!");
+
+}
+
+
+
+
+
+
+alert("El segundo número que ingresaste es
+positivo, Mayor o igual que 0");
+}
+if(num1 != 0 || num1 < 0) {
+alert("El primer número que ingresaste es
+distinto de 0 o es negativo");
+}
+
+  // Usamos document.write para agregar contenido al div con id="contenido"
+  document.write('<h1>Hola Mundo!</h1>');
+  document.write('<p>Este es un ejemplo de cómo modificar el HTML con JavaScript utilizando document.write.</p>');
